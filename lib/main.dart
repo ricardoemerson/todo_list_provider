4dcd5331ec71a91aa5fe02607d74e764531952b2
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2022-07-11 14:16:14 -0300

    Setup of firebase_auth.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:wakelock/wakelock.dart';
 
 import 'app/app_module.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   Wakelock.enable();
 
